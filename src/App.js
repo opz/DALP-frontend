1087ddc70beca613f7711b5f03ae220f21aaf473
Sebastian Stant <sebastianstant@gmail.com>
2020-05-10 09:35:25 -0400

    added web3modal

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React from 'react';
+import Header from './components/layout/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="container">
+        <h1>Welcome to DALP.</h1>
+      </div>
     </div>
   );
 }

4f80e44c451907a01cdf8ec4bf109c2b38f2f6fa
Mitchell Opatowsky <opatowsky77@gmail.com>
2020-05-22 18:11:05 -0400

    local checkin

---
+++ src/components/elements/Chart/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Row, Col } from "reactstrap";
+import cn from "classnames";
+import * as align from "../../Constants/alignments";
+
+import { Doughnut } from "react-chartjs-2";
+
+export default function () {
+  return <div></div>;
+}

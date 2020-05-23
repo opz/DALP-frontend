dde4df2bb87b9cd999f5c47866b5481230bdf099
opz <will.shahda@gmail.com>
2020-05-23 17:58:35 -0400

    Remove unused Fragment import

--- src/components/elements/DemoSwitch.jsx
+++ src/components/elements/DemoSwitch.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useContext } from "react";
+import React, { useContext } from "react";
 import styled from "styled-components";
 import Switch from "react-switch";
 import { DataContext } from "../../providers/data";

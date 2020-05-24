826c9fe995b505273520fd863919b5efd2716459
opz <will.shahda@gmail.com>
2020-05-24 00:47:51 -0400

    Handle non-standard MKR ERC-20 ABI
    Fixes the previous commit's overflow error. MKR returned a bytes32 as
    the symbol. The string was stored in the most significant bytes of
    the data, making it not only interpreted as a number by web3, but a huge
    one.

--- src/components/elements/PoolAllocation.jsx
+++ src/components/elements/PoolAllocation.jsx
@@ -5,6 +5,8 @@ import { WalletContext } from "../../providers/wallet";
 import ERC20 from "dalp-core/artifacts/ERC20.json";
 import { zeroAddress } from "../../Constants/ethereum";
 
+const mkrAbi = [ { "inputs": [], "name": "name", "outputs": [ { "internalType": "bytes32", "name": "", "type": "bytes32" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "symbol", "outputs": [ { "internalType": "bytes32", "name": "", "type": "bytes32" } ], "stateMutability": "view", "type": "function" }, ];
+
 const StyledRow = styled.div`
   border-bottom: 1px solid rgba(0,0,0,.1);
   margin-bottom: 20px;
@@ -34,37 +36,44 @@ const PoolAllocation = () => {
 
   useEffect(() => {
     if (dalpManager) {
-      dalpManager.methods.getActiveUniswapV2Tokens().call().then(({ 0: token0, 1: token1 }) => {
-        setToken0(token0);
-        setToken1(token1);
+      dalpManager.methods.getActiveUniswapV2Tokens().call().then(tokens => {
+        setToken0(tokens[0]);
+        setToken1(tokens[1]);
       });
     }
   }, [dalpManager]);
 
   useEffect(() => {
     if (token0 !== zeroAddress && token1 !== zeroAddress) {
+      // MKR's Kovan deployment has a wack ABI
+      const mkrAddress = "0xAaF64BFCC32d0F15873a02163e7E500671a4ffcD";
+      const token0Abi = token0 === mkrAddress ? mkrAbi : ERC20.abi;
+      const token1Abi = token1 === mkrAddress ? mkrAbi : ERC20.abi;
+
       dalpManager.methods.getDalpProportionalReserves().call()
-        .then(({0: reserve0, 1: reserve1}) => {
-          const token0Contract = new wallet.eth.Contract(ERC20.abi, token0);
+        .then(reserves => {
+          const token0Contract = new wallet.eth.Contract(token0Abi, token0);
           Promise.all([
             token0Contract.methods.symbol().call(),
             token0Contract.methods.name().call()
-          ]).then(([balance0, symbol0, name0]) => {
+          ]).then(([symbol0, name0]) => {
             setToken0Data({
-              "balance": reserve0,
-              "symbol": symbol0,
+              "balance": reserves[0],
+              // Handle case where symbol is a bytes32, such as the MKR token
+              "symbol": wallet.utils.isHex(symbol0) ? wallet.utils.hexToUtf8(symbol0) : symbol0,
               "name": name0
             });
           });
 
-          const token1Contract = new wallet.eth.Contract(ERC20.abi, token1);
+          const token1Contract = new wallet.eth.Contract(token1Abi, token1);
           Promise.all([
             token1Contract.methods.symbol().call(),
             token1Contract.methods.name().call()
-          ]).then(([balance1, symbol1, name1]) => {
+          ]).then(([symbol1, name1]) => {
             setToken1Data({
-              "balance": reserve1,
-              "symbol": symbol1,
+              "balance": reserves[1],
+              // Handle case where symbol is a bytes32, such as the MKR token
+              "symbol": wallet.utils.isHex(symbol1) ? wallet.utils.hexToUtf8(symbol1) : symbol1,
               "name": name1
             });
           });

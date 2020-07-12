154df889eaa0c50f75a076bf511823bb23388bd2
opz <will.shahda@gmail.com>
2020-07-12 18:02:01 -0400

    Enforce Kovan network for transactions page

--- src/components/pages/Transactions.jsx
+++ src/components/pages/Transactions.jsx
@@ -5,25 +5,44 @@ import ActionForms from "../elements/Form";
 import { WalletContext } from "../../providers/wallet";
 
 const Transactions = () => {
-  const { account } = useContext(WalletContext);
-  return (
-    <div id="page">
-      <div className="container">
-        <div className="row">
-          {account ? (
-            <div className="col-md-4">
-              <BalanceCard />
-              <ActionForms />
+  const { networkId, account } = useContext(WalletContext);
+  if (networkId !== 42) {
+    return (
+      <div id="page">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-xs-12">
+              <p className="h6 text-center">
+                APY.Finance is only available on the Kovan network.
+              </p>
+              <p className="h6 text-center">
+                Please switch MetaMask to the Kovan network.
+              </p>
             </div>
-          ) : (
-            <h1>Loading...</h1>
-          )}
-          <div className="col-md-8">
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div id="page">
+        <div className="container">
+          <div className="row">
+            {account ? (
+              <div className="col-md-4">
+                <BalanceCard />
+                <ActionForms />
+              </div>
+            ) : (
+              <h1>Loading...</h1>
+            )}
+            <div className="col-md-8">
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default Transactions;

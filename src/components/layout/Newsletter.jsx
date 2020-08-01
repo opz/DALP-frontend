75c51e16f3867d742f284590fd6f36d8999affe9
“dinadeljanin” <ddeljanin5@icloud.com>
2020-08-01 17:50:49 -0400

    added medium link to the footer // changed header style and text for newsletter

--- src/components/layout/Newsletter.jsx
+++ src/components/layout/Newsletter.jsx
@@ -6,7 +6,7 @@ export default class Newsletter extends Component {
       <div className="subscribe-container" id="mc_embed_signup">
         <form action="https://finance.us17.list-manage.com/subscribe/post?u=364c817c91a8f54732894b7c6&amp;id=4462c6d940" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
           <div id="mc_embed_signup_scroll">
-            <h2>Subscribe to the Mailing List</h2>
+            <h1>Register for Alpha Launch</h1>
             <div className="field-wrapper">
               <input type="email" defaultValue="" name="EMAIL" className="email-input" id="mce-EMAIL" placeholder="Your Email Address" autoComplete="off" required />
               <div style={{position: "absolute", left: "-5000px"}} aria-hidden="true">

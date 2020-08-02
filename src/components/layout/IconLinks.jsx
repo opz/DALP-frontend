293d96e0c7c66edf7e7f021d577d24e80f97899f
opz <will.shahda@gmail.com>
2020-08-02 15:06:09 -0400

    Open footer links in new tab

--- src/components/layout/IconLinks.jsx
+++ src/components/layout/IconLinks.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const IconLink = ({icon, link}) => (
   <div className="icon-link-wrapper">
-    <a href={link} className="icon-link">
+    <a target="_blank" rel="noopener noreferrer" href={link} className="icon-link">
       <div className="icon-image" style={{backgroundImage: `url(${icon})`}}></div>
     </a>
   </div>

5c64d9523024cfe9d5cb099291bf4132f74d82a5
“dinadeljanin” <ddeljanin5@icloud.com>
2020-08-06 19:30:03 -0400

    Updated team section -- Added Chan-Ho

--- src/components/layout/Team.jsx
+++ src/components/layout/Team.jsx
@@ -13,6 +13,7 @@ import sunil from "../../assets/images/team-sunil.jpeg";
 import dina from "../../assets/images/team-dina.jpg";
 import shun from "../../assets/images/team-shun.jpeg";
 import pascal from "../../assets/images/team-pascal.jpeg";
+import chanho from "../../assets/images/team-chanho.jpg"
 
 const Team = () => {
   return (
@@ -31,22 +32,32 @@ const Team = () => {
         </Col>
         <Col xs="6" md="4" className={cn(topCenter, noMarginPad)}>
           <div className={cn(full, allCenter, noMarginPad)}>
-            <img src={dina} alt="sunil" className={cn("team-graphic")} />
+            <img src={chanho} alt="sunil" className={cn("team-graphic")} />
           </div>
           <p className={cn("mt-3", "text-center")}>
-            <b>Dina Deljanin</b>
+            <b>Chan-Ho Suh</b>
             <br />
-            Front-End Engineer
+            Solidity Engineer
           </p>
         </Col>
         <Col xs="6" md="4" className={cn(topCenter, noMarginPad)}>
           <div className={cn(full, allCenter, noMarginPad)}>
-            <img src={shun} alt="sunil" className={cn("team-graphic")} />
+            <img src={shun} alt="shun" className={cn("team-graphic")} />
           </div>
           <p className={cn("mt-3", "text-center")}>
             <b>Shun Bhark</b>
             <br />
             Full-Stack Engineer
+          </p>
+        </Col>
+        <Col xs="6" md="4" className={cn(topCenter, noMarginPad)}>
+          <div className={cn(full, allCenter, noMarginPad)}>
+            <img src={dina} alt="dina" className={cn("team-graphic")} />
+          </div>
+          <p className={cn("mt-3", "text-center")}>
+            <b>Dina Deljanin</b>
+            <br />
+            Front-End Engineer
           </p>
         </Col>
       </Row>
